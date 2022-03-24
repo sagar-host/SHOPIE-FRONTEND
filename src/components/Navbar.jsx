2988a2e749868ea2eb0c94f37cd7a74768cfd152
sagar <rebelsagarx@gmail.com>
2022-03-24 22:21:45 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,124 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  height: 60px;
+  ${mobile({ height: "60px" })}
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: "0px 0px" })}
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+  ${mobile({ display: "none" })}
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+  ${mobile({ width: "50px" })}
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })};
+  &:hover{
+    color: whitesmoke;
+    transform: scale(1.1);
+}
+`;
+
+const Navbar = () => {
+  const quantity = useSelector(state=>state.cart.quantity)
+
+  
+  const user = useSelector((state) => state.user.currentUser);
+ 
+  const logout = () => {
+    
+    localStorage.removeItem('persist:root')
+    window.location.reload()
+   
+  
+  }
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search" />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo><Link to='/' style={{textDecoration:"none",color:"black"}}>SHOPIE.</Link></Logo>
+        </Center>
+        <Right>
+        {!user? <>    
+                    <MenuItem><Link to='/register' style={{'textDecoration': 'none','color':'#b1b1b1'}}>REGISTER</Link></MenuItem>
+
+                     <MenuItem ><Link to='/login' style={{'textDecoration': 'none','color':'#b1b1b1'}}>Sign In</Link></MenuItem>
+                     </>
+:                     
+  <MenuItem ><Link to='/' style={{'textDecoration': 'none','color':'#b1b1b1'}} value='Reload Page' onClick={logout} >Sign Out</Link></MenuItem>
+         
+ }
+          <Link to="/cart">
+          <MenuItem>
+            <Badge badgeContent={quantity} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+          </Link>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
